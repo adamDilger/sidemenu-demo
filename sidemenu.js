34b4e4ece558ba250dcae9569a102721fee28c79
Adam Dilger <adamdilger1@gmail.com>
2018-12-04 21:58:39 +1100

    initial commit

---
+++ sidemenu.js
@@ -0,0 +1,32 @@
+let card1, card2;
+
+document.addEventListener('DOMContentLoaded', function() {
+  card1 = document.getElementById('card1');
+  card2 = document.getElementById('card2');
+}, false);
+
+function cardOneClick() {
+  let add = card1;
+  let remove = card2;
+
+  if (card1.classList.contains('card-large')) {
+    remove = card1;
+  } else {
+    add.classList.add('card-large');
+  }
+
+  remove.classList.remove('card-large');
+}
+
+function cardTwoClick() {
+  let add = card2;
+  let remove = card1;
+
+  if (card2.classList.contains('card-large')) {
+    remove = card2;
+  } else {
+    add.classList.add('card-large');
+  }
+
+  remove.classList.remove('card-large');
+}
